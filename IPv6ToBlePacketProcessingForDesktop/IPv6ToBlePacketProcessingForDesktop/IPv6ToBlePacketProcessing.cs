4f0a9aeb19900787d99a44acd7dda2356c11db3e
Duncan MacMichael <duncanmacmichael@outlook.com>
2018-04-09 01:05:27 -0700

    Replacing background UWP app with Windows Service for desktop packet processing, removed IoT packet processing for now

---
+++ IPv6ToBlePacketProcessingForDesktop/IPv6ToBlePacketProcessingForDesktop/IPv6ToBlePacketProcessing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPv6ToBlePacketProcessingForDesktop
+{
+    public partial class IPv6ToBlePacketProcessing : ServiceBase
+    {
+        public IPv6ToBlePacketProcessing()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void process1_Exited(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
